05e62e15802d9ac3c2fea2a2682be679eaa5b1df
df-Nisar248 <mahammadn143@gmail.com>
2022-05-27 10:06:41 +0530

    latest

--- virhost_lncr/settings.py
+++ virhost_lncr/settings.py
@@ -34,7 +34,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'virhost_lncr.urls'
@@ -112,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
